115070cefa1a025d3fe6b7651313dc007fbb1061
Adrià Pons Mensa <adria.pons.mensa@estudiantat.upc.edu>
2024-01-11 09:37:19 +0100

    BB Cop&Robber finished !

--- GameAI_FinalProject/Assets/Scripts/BB/MouseMove.cs
+++ GameAI_FinalProject/Assets/Scripts/BB/MouseMove.cs
@@ -2,19 +2,29 @@
 using UnityEngine.AI;
 
 public class MouseMove : MonoBehaviour {
-	private NavMeshAgent agent;
+    private NavMeshAgent agent;
+    public Camera selectedCamera; // Variable to store the specific camera
 
-	void Start () {
-		agent = GetComponent<NavMeshAgent> ();
-	}
-	
-	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
-			RaycastHit hit;
-			Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
-			if (Physics.Raycast (camRay, out hit, 100)) {
-				agent.destination = hit.point;
-			}
-		}
-	}
+    void Start () {
+        agent = GetComponent<NavMeshAgent> ();
+    }
+
+    void Update () {
+        if (Input.GetMouseButtonDown (0)) {
+            RaycastHit hit;
+            Ray camRay;
+
+            if (selectedCamera != null) {
+                // If a specific camera is selected, use it
+                camRay = selectedCamera.ScreenPointToRay (Input.mousePosition);
+            } else {
+                // If no specific camera is selected, use the main camera
+                camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+            }
+
+            if (Physics.Raycast (camRay, out hit, 100)) {
+                agent.destination = hit.point;
+            }
+        }
+    }
 }
